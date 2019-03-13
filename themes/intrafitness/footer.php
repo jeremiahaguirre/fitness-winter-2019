968d5707008c2bc6ea9f0291d8f8c2b51b0b65b1
Jeremiah <happytobike@gmail.com>
2019-03-13 13:58:20 -0700

    Made the rough mark up for footer

--- themes/intrafitness/footer.php
+++ themes/intrafitness/footer.php
@@ -7,16 +7,25 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        <nav id="site-navigation" class="footer-navigation" role="navigation">
+            <?php wp_nav_menu(array('menu'=> "footer-menu", 'menu_id' => 'footer-menu')); ?>
+        </nav><!-- footer-navigation -->
+        <div class="mobile disclaimers">
+            <p>Privacy Policies</p>
+            <p>Terms and Conditions</p>
+            <p>Disclaimer</p>
+        </div>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
+
 </html>
+
