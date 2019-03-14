82940cdf2ab3367bfda679a5f8a9c920161bd01a
Jeremiah <happytobike@gmail.com>
2019-03-13 18:35:01 -0700

    Changed where the logo is located in header

--- themes/intrafitness/header.php
+++ themes/intrafitness/header.php
@@ -23,9 +23,10 @@
         <a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
         <header id="masthead" class="site-header" role="banner">
-            <a class="logo" href="<?php bloginfo('url'); ?>">
-            </a>
+
             <div class="site-branding">
+                <a class="logo" href="<?php bloginfo('url'); ?>">
+                </a>
                 <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
             </div><!-- .site-branding -->
 
