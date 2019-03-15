d6df2d3b6153850a40bc572316ff0d4c11f0fba9
Jeremiah <happytobike@gmail.com>
2019-03-15 10:36:54 -0700

    Made the mobile width drop down menu responsive

--- themes/intrafitness/js/menu.js
+++ themes/intrafitness/js/menu.js
@@ -6,6 +6,13 @@
         duration: 'fast',
         start: function() {
           $(this).css({ display: 'flex' });
+        },
+        done: function() {
+          if ($('#primary-menu').is(':visible')) {
+            $('body').addClass('menu-show');
+          } else {
+            $('body').removeClass('menu-show');
+          }
         }
       });
     });
