e58013de99e42e67657057f698010f3715d79875
Andre Moura <andremarquesmoura@gmail.com>
2019-03-18 11:46:57 -0700

    Set home.php to show the default post type (home.php)

--- themes/intrafitness/home.php
+++ themes/intrafitness/home.php
@@ -10,39 +10,29 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
+        <?php if (have_posts()) : ?>
+
+        <?php if (is_home() && !is_front_page()) : ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+        </header>
+        <?php endif; ?>
+
+        <?php  /* Start the Loop */ ?>
         <?php while (have_posts()) : the_post(); ?>
 
-        <?php get_template_part('template-parts/content', 'page'); ?>
+        <?php get_template_part('template-parts/content'); ?>
 
         <?php endwhile; ?>
 
-        <?php
-$args = array('post_type' => 'post_testimonial', 'posts_per_page' => 3, 'order'=>'DESC');
-$testimonials = get_posts($args);
-?>
+        <?php the_posts_navigation(); ?>
 
-<?php foreach ($testimonials as $post) : setup_postdata($post); ?>
-<article>
-<?php the_title(); ?>
-<?php the_content(); ?>
-</article>
-<?php endforeach;
-        wp_reset_postdata(); ?>
+        <?php else : ?>
 
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
-        <?php
-    $args = array('post_type' => 'post_faq', 'posts_per_page' => 3, 'order'=>'DESC');
-    $faq = get_posts($args);
-    ?>
+        <?php endif; ?>
 
-    <?php foreach ($faq as $post) : setup_postdata($post); ?>
-    <article>
-    <?php the_title(); ?>
-    <?php the_content(); ?>
-    </article>
-    <?php endforeach;
-    wp_reset_postdata(); ?>
-        
     </main><!-- #main -->
 </div><!-- #primary -->
 
