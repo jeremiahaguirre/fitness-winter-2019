3b4c1e3f5884977ce1adbb1ab1abf13fffa8eb9e
Bryan Tran <bryant.business@hotmail.com>
2019-03-18 11:45:48 -0700

    removed uneeded code home.php

--- themes/intrafitness/home.php
+++ themes/intrafitness/home.php
@@ -5,6 +5,7 @@
  * @package IntraFitness_Theme
  */
 
+ 
 get_header(); ?>
 
 <div id="primary" class="content-area">
@@ -16,33 +17,6 @@ get_header(); ?>
 
         <?php endwhile; ?>
 
-        <?php
-$args = array('post_type' => 'post_testimonial', 'posts_per_page' => 3, 'order'=>'DESC');
-$testimonials = get_posts($args);
-?>
-
-<?php foreach ($testimonials as $post) : setup_postdata($post); ?>
-<article>
-<?php the_title(); ?>
-<?php the_content(); ?>
-</article>
-<?php endforeach;
-        wp_reset_postdata(); ?>
-
-
-        <?php
-    $args = array('post_type' => 'post_faq', 'posts_per_page' => 3, 'order'=>'DESC');
-    $faq = get_posts($args);
-    ?>
-
-    <?php foreach ($faq as $post) : setup_postdata($post); ?>
-    <article>
-    <?php the_title(); ?>
-    <?php the_content(); ?>
-    </article>
-    <?php endforeach;
-    wp_reset_postdata(); ?>
-        
     </main><!-- #main -->
 </div><!-- #primary -->
 
