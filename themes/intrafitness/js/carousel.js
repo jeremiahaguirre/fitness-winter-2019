35d336a237618ff9ab4b47986217a8507004bae7
Jeremiah <happytobike@gmail.com>
2019-03-21 16:44:10 -0700

    styled the mobile testimonys and added flickity

--- themes/intrafitness/js/carousel.js
+++ themes/intrafitness/js/carousel.js
@@ -1,13 +1,31 @@
-(function($){
-
-$(document).ready(function(){
-
+(function($) {
+  $(function() {
     $('.blog-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-      });
-
-});
-
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
+    //testimonial carosel
+    let flickityEnabled = false;
+    function testimonyCarosel() {
+      if ($(window).width() < 640) {
+        $('#tesitmonial-carosel').flickity({
+          freeScroll: true,
+          cellAlign: 'left',
+          prevNextButtons: false,
+          pageDots: false
+        });
+        flickityEnabled = true;
+      } else {
+        if (flickityEnabled) {
+          $('#tesitmonial-carosel').flickity('destroy');
+          flickityEnabled = false;
+        }
+      }
+    }
+    testimonyCarosel();
+    $(window).on('resize', () => {
+      testimonyCarosel();
+    });
+  });
 })(jQuery);
